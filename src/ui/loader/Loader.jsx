28d75ec94967f2d66c2838ab86de672ea1bc2271
MD Masud Rana <66163632+mdmasudranainfo@users.noreply.github.com>
2024-04-04 15:03:07 +0600

    branch

--- src/ui/loader/Loader.jsx
+++ src/ui/loader/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center w-full h-[90vh]">
       <div className="w-10 h-10 animate-[spin_2s_linear_infinite] rounded-full border-8 border-dotted border-sky-600"></div>
     </div>
   );
