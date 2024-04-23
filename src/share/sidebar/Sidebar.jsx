58f4f9cec90bbd8e75962b945680df29a9469f5b
nhidhossain <120573052+nhidhossain@users.noreply.github.com>
2024-04-23 18:02:52 +0600

    softxbd sidebar

--- src/share/sidebar/Sidebar.jsx
+++ src/share/sidebar/Sidebar.jsx
@@ -5,23 +5,31 @@ const Sidebar = ({ active, setActive }) => {
   const toggleSitebarValue = useBooleanStore(
     (state) => state.toggleSitebarValue
   );
-  console.log(active);
+
   return (
     <>
-      <div className="">
+      <div className=" flex">
         <div
-          className={`${active ? "hidden w-[0px]" : " block"}  duration-700`}
+          className={`${
+            active
+              ? "xl:w-0 bg-primary-muted "
+              : "xl:w-[250px] overflow-hidden h-screen"
+          }  duration-500 !overflow-hidden `}
         >
           <div
             className={`${
               toggleSitebarValue
-                ? "block w-full sm:w-[300px] left-0 fixed"
+                ? "block w-full xl:w-[250px] left-0 fixed z-50 "
                 : "hidden"
             } 
-           -ml-[3px] left-0  2xl:relative bg-white 2xl:block 2xl:w-[300px] top-14 xl:-top-2 overflow-hidden z-20 `}
+           -ml-[3px] left-0 xl:relative bg-white xl:block xl:w-[250px] top-14 xl:-top-2 overflow-hidden z-20 `}
           >
             <div className="flex gap-2">
-              <div className={` bg-primary-muted w-[300px] fixed h-full pt-4`}>
+              <div
+                className={` ${
+                  active ? "w-0 hidden" : " w-[250px]"
+                }  bg-primary-muted w-[250px] fixed h-full pt-4 z-50 `}
+              >
                 <div className="flex">
                   <div
                     className={`  w-full space-y-2 h-[90vh] sidebarScrool  overflow-auto  scroll-m-0 pr-2`}
@@ -30,21 +38,15 @@ const Sidebar = ({ active, setActive }) => {
                       <SidebarComponents key={i} item={item} />
                     ))}
                   </div>
-                  <div className="flex  h-[90vh] items-center">
-                    <div
-                      onClick={() => setActive(!active)}
-                      className="w-[8px] h-[150px] bg-[#f3f3f34a] rounded-md cursor-pointer"
-                    ></div>
-                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className=" hidden md:flex  h-[90vh] items-center">
+        <div className=" hidden  xl:flex  group h-[90vh] items-center">
           <div
             onClick={() => setActive(!active)}
-            className="w-[8px] h-[150px] bg-[#f3f3f34a] rounded-md cursor-pointer"
+            className="w-[8px] h-[100px] group-hover:h-[130px] duration-300 bg-[#f3f3f34a] rounded-md cursor-pointer"
           ></div>
         </div>
       </div>
